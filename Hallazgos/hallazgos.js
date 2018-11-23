4bc0030551587936171f3812db66ef00b747a121
Jhon Arias <jhonarmont@hotmail.com>
2018-11-23 12:33:29 -0500

    Applied bootstrap 4 to the form

--- Hallazgos/hallazgos.js
+++ Hallazgos/hallazgos.js
@@ -12,19 +12,19 @@ function AlertaValidacionCampos() {
         'Hay campos vacíos!',
         'Recuerda llenar todos los campos requeridos(*) del formulario!',
         'warning'
-    )
+    );
 }
 function AlertaAvance() {
     swal(
         'Avance mayor a 100!',
         'Recuerda que el avance no puede ser mayor a 100',
         'warning'
-    )
+    );
 }
 function AlertaSuccess() {
     swal(
         'Guardado!',
         'Bien Hecho',
         'success'
-    )
+    );
 }
